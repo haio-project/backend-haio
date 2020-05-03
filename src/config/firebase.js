3a4cc771498baac15c3d8b7681175b0a7d848875
lfp2 <lfp2@cin.ufpe.br>
2020-05-03 17:19:00 -0300

    Fixing env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,7 @@
 const admin = require("firebase-admin");
-const firebaseFile = require("./firebase.json");
 
 const firebaseAdminConfig = admin.initializeApp({
-  credential: admin.credential.cert(firebaseFile),
+  credential: admin.credential.cert(process.env.FIREBASE_CONFIG),
   databaseURL: "https://haio-f6f4b.firebaseio.com",
 });
 
